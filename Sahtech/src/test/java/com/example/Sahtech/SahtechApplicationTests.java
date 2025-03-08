31c29259be269130409aa97dcf2df6d8d01aa73b
Idris_ch <cherietidrisse1@gmail.com>
2025-03-08 10:11:51 +0100

    Added files to repository

---
+++ Sahtech/src/test/java/com/example/Sahtech/SahtechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sahtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SahtechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
