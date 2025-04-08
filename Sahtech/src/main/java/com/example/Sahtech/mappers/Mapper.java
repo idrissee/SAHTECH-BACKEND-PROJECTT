ab6d5907143e75b9a5730b4be26ad0888d8bf46a
fouadbkh74 <155484599+fouadbkh74@users.noreply.github.com>
2025-04-08 19:55:57 +0200

    co

--- Sahtech/src/main/java/com/example/Sahtech/mappers/Mapper.java
+++ Sahtech/src/main/java/com/example/Sahtech/mappers/Mapper.java
@@ -1,8 +1,8 @@
 package com.example.Sahtech.mappers;
 
 public interface Mapper<A,B> {
-    B mapTo(A a);
 
-    A mapFrom(B b);
+    B mapTo(A a); // DTO TO ENTITEE
+    A mapFrom(B b); // LE CONTRAIRE
 }
 
